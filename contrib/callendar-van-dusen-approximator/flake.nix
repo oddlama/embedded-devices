33a8811ff62165bf8e0ffd71d8c4823c1ddf3fb5
oddlama <oddlama@oddlama.org>
2024-08-22 13:22:20 +0200

    feat: extend approximator to 4th order polynomials

--- contrib/callendar-van-dusen-approximator/flake.nix
+++ contrib/callendar-van-dusen-approximator/flake.nix
@@ -37,6 +37,7 @@
                 p:
                   with p; [
                     torchWithCuda
+                    scipy
                     numpy
                     matplotlib
                   ]
--- contrib/callendar-van-dusen-approximator/main.py
+++ contrib/callendar-van-dusen-approximator/main.py
@@ -3,8 +3,9 @@
 # analytical solution:
 # t = -1/2 sqrt((2.25689e-20 (14123424280463317916104064499712 y - 15409551594649655757462803841024 K))/(477244327538622144 K^3 - 42810999909758488 K^2 y + 1.69526e-30 K^(3/2) sqrt(93888020965527858323701368714067466232986277692359736916684766077860651474130183296704764182528 K^3 - 54462502494335639718685321165686266165384797958199188415987855307271839840242508360456431730688 K^2 y + 37522856856107613087317656212462989703632371986859477818291109746388036214224728942788644700160 K y^2 - 11268860665316324488520037218937802013667426589820931462978601746317432642597497101502847647744 y^3))^(1/3) + (477244327538622144 K^3 - 42810999909758488 K^2 y + 1.69526e-30 K^(3/2) sqrt(93888020965527858323701368714067466232986277692359736916684766077860651474130183296704764182528 K^3 - 54462502494335639718685321165686266165384797958199188415987855307271839840242508360456431730688 K^2 y + 37522856856107613087317656212462989703632371986859477818291109746388036214224728942788644700160 K y^2 - 11268860665316324488520037218937802013667426589820931462978601746317432642597497101502847647744 y^3))^(1/3)/K - 89539.2) - 1/2 sqrt(-(2.25689e-20 (14123424280463317916104064499712 y - 15409551594649655757462803841024 K))/(477244327538622144 K^3 - 42810999909758488 K^2 y + 1.69526e-30 K^(3/2) sqrt(93888020965527858323701368714067466232986277692359736916684766077860651474130183296704764182528 K^3 - 54462502494335639718685321165686266165384797958199188415987855307271839840242508360456431730688 K^2 y + 37522856856107613087317656212462989703632371986859477818291109746388036214224728942788644700160 K y^2 - 11268860665316324488520037218937802013667426589820931462978601746317432642597497101502847647744 y^3))^(1/3) - (477244327538622144 K^3 - 42810999909758488 K^2 y + 1.69526e-30 K^(3/2) sqrt(93888020965527858323701368714067466232986277692359736916684766077860651474130183296704764182528 K^3 - 54462502494335639718685321165686266165384797958199188415987855307271839840242508360456431730688 K^2 y + 37522856856107613087317656212462989703632371986859477818291109746388036214224728942788644700160 K y^2 - 11268860665316324488520037218937802013667426589820931462978601746317432642597497101502847647744 y^3))^(1/3)/K - (1.8551e9)/sqrt((2.25689e-20 (14123424280463317916104064499712 y - 15409551594649655757462803841024 K))/(477244327538622144 K^3 - 42810999909758488 K^2 y + 1.69526e-30 K^(3/2) sqrt(93888020965527858323701368714067466232986277692359736916684766077860651474130183296704764182528 K^3 - 54462502494335639718685321165686266165384797958199188415987855307271839840242508360456431730688 K^2 y + 37522856856107613087317656212462989703632371986859477818291109746388036214224728942788644700160 K y^2 - 11268860665316324488520037218937802013667426589820931462978601746317432642597497101502847647744 y^3))^(1/3) + (477244327538622144 K^3 - 42810999909758488 K^2 y + 1.69526e-30 K^(3/2) sqrt(93888020965527858323701368714067466232986277692359736916684766077860651474130183296704764182528 K^3 - 54462502494335639718685321165686266165384797958199188415987855307271839840242508360456431730688 K^2 y + 37522856856107613087317656212462989703632371986859477818291109746388036214224728942788644700160 K y^2 - 11268860665316324488520037218937802013667426589820931462978601746317432642597497101502847647744 y^3))^(1/3)/K - 89539.2) - 179078.) + 25
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import scipy
 
 # Constants for Callendar-Van Dusen equation (Platinum RTD, typical values)
 R0 = 100  # Resistance at 0°C
@@ -19,6 +20,28 @@ def resistance(t):
     else:
         return R0 * (1 + A * t + B * t**2)
 
+def approx_order_1(r, r1, t1, r2, t2):
+    return np.interp(r, [r1, r2], [t1, t2])
+
+def parabola(r, a, b, c):
+    return a * r**2 + b * r + c
+
+def cubic(r, a, b, c, d):
+    return a * r**3 + b * r**2 + c * r + d
+
+def o4(r, a, b, c, d, e):
+    return a * r**4 + b * r**3 + c * r**2 + d * r + e
+
+def o5(r, a, b, c, d, e, f):
+    return e * r**5 + a * r**4 + b * r**3 + c * r**2 + d * r + e
+
+def o6(r, a, b, c, d, e, f, g):
+    return f * r**6 + e * r**5 + a * r**4 + b * r**3 + c * r**2 + d * r + e
+Fs = [parabola, cubic, o4, o5, o6]
+Frange = list(range(2, 2 + len(Fs)))
+
+approx_order = 4
+
 print("preparing values")
 # Generate equidistant temperature positions
 n_samples = 500_000
@@ -31,7 +54,7 @@ print("done preparing values")
 
 # Function to generate lookup table
 def generate_lookup_table(min_temp, max_temp, desired_max_err):
-    lookup_table = [(0, resistances[0], temperatures[0])]  # Start with first entry
+    lookup_table = [(0, resistances[0], temperatures[0], None)]  # Start with first entry
     first_index = 1
 
     while first_index < n_samples - 1:
@@ -41,7 +64,13 @@ def generate_lookup_table(min_temp, max_temp, desired_max_err):
 
         # Binary search to find the best position
         while True:
-            f_approx = np.vectorize(lambda r: np.interp(r, [resistances[first_index], resistances[current_end_index]], [temperatures[first_index], temperatures[current_end_index]]))
+            fit_params = None
+            if approx_order in Frange:
+                F = Fs[approx_order - 2]
+                fit_params, pcov = scipy.optimize.curve_fit(F, resistances[first_index:current_end_index+1], temperatures[first_index:current_end_index+1])
+                f_approx = np.vectorize(lambda r: F(r, *fit_params))
+            else:
+                f_approx = np.vectorize(lambda r: approx_order_1(r, resistances[first_index], temperatures[first_index], resistances[current_end_index], temperatures[current_end_index]))
             # print(f"Check range [{first_index},{current_end_index}]")
 
             approx_temps = f_approx(resistances[first_index:current_end_index+1])
@@ -68,7 +97,7 @@ def generate_lookup_table(min_temp, max_temp, desired_max_err):
             next_change = next_change // 2
 
         # Add the best position to the lookup table
-        lookup_table.append((current_end_index+1, resistances[current_end_index], temperatures[current_end_index]))
+        lookup_table.append((current_end_index+1, resistances[current_end_index], temperatures[current_end_index], fit_params))
         first_index = current_end_index + 1
 
     return lookup_table
@@ -76,11 +105,21 @@ def generate_lookup_table(min_temp, max_temp, desired_max_err):
 # Define parameters for the lookup table generation
 min_temp = -200
 max_temp = 900
-max_err = 0.001  # Desired maximum error in °C
+max_err = 0.00001  # Desired maximum error in °C
 
 # Generate the lookup table
 lookup_table = generate_lookup_table(min_temp, max_temp, max_err)
-approx_temps = np.interp(resistances, [x[1] for x in lookup_table], [x[2] for x in lookup_table])
+if approx_order in Frange:
+    F = Fs[approx_order - 2]
+    approx_temps = []
+    for k,(li,lr,lt,_) in enumerate(lookup_table[0:-1]):
+        ri,rr,rt,params = lookup_table[k+1]
+        f_approx = np.vectorize(lambda r: F(r, *params))
+        approx_temps.append(f_approx(resistances[li:ri]))
+    approx_temps = np.concatenate(approx_temps, axis=0)
+elif approx_order == 1:
+    approx_temps = np.interp(resistances, [x[1] for x in lookup_table], [x[2] for x in lookup_table])
+
 error = approx_temps - temperatures
 # abs_error = np.abs(approx_temps - temperatures)
 max_error = np.max(np.abs(approx_temps - temperatures))
@@ -88,8 +127,8 @@ print(f"Table calculation done!")
 print(f"Entries:       ", len(lookup_table))
 print(f"Maximum Error: {max_error:.4f}", )
 print(f"Mean Error:    {error.mean():.4f}")
-for i,r,t in lookup_table:
-    print(f"{i:20},{r:20},{t:20}")
+for i,r,t,params in lookup_table:
+    print(f"{i:20},{r:20},{t:20},{params}")
 
 # Create the plot
 fig, ax1 = plt.subplots(figsize=(10, 6))
