b1d616ad334a78ecbc9694d7cc532108524804bf
oddlama <oddlama@oddlama.org>
2024-11-22 14:49:36 +0100

    feat: update flake and add treefmt and numtide/devshell

--- flake.nix
+++ flake.nix
@@ -1,20 +1,33 @@
 {
   inputs = {
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nci.url = "github:yusdacra/nix-cargo-integration";
+    nci = {
+      url = "github:yusdacra/nix-cargo-integration";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
+        inputs.devshell.flakeModule
         inputs.nci.flakeModule
         inputs.pre-commit-hooks.flakeModule
+        inputs.treefmt-nix.flakeModule
       ];
 
       systems = [
@@ -22,36 +35,43 @@
         "aarch64-linux"
       ];
 
-      perSystem = {
-        config,
-        pkgs,
-        ...
-      }: let
-        projectName = "embedded-devices";
-      in {
-        pre-commit.settings.hooks = {
-          alejandra.enable = true;
-          deadnix.enable = true;
-          statix.enable = true;
-        };
-
-        nci.projects.${projectName}.path = ./.;
-        nci.crates.${projectName} = {};
-
-        devShells.default = config.nci.outputs.${projectName}.devShell.overrideAttrs (old: {
-          nativeBuildInputs =
-            (old.nativeBuildInputs or [])
-            ++ [
+      perSystem =
+        {
+          config,
+          pkgs,
+          ...
+        }:
+        let
+          projectName = "embedded-devices";
+        in
+        {
+          devshells.default = {
+            packages = [
+              config.treefmt.build.wrapper
               pkgs.cargo-release
             ];
-          shellHook = ''
-            ${old.shellHook or ""}
-            ${config.pre-commit.installationScript}
-          '';
-        });
+            devshell.startup.pre-commit.text = config.pre-commit.installationScript;
+          };
 
-        packages.default = config.nci.outputs.${projectName}.packages.release;
-        formatter = pkgs.alejandra; # `nix fmt`
-      };
+          pre-commit.settings.hooks.treefmt.enable = true;
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              deadnix.enable = true;
+              statix.enable = true;
+              nixfmt.enable = true;
+              rustfmt.enable = true;
+            };
+          };
+
+          nci.projects.${projectName} = {
+            path = ./.;
+            numtideDevshell = "default";
+          };
+          nci.crates.${projectName} = { };
+
+          packages.default = config.nci.outputs.${projectName}.packages.release;
+        };
     };
 }
