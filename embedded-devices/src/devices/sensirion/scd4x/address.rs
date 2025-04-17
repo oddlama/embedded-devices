2c558184310980bc9b40f6ea744db9d36454b8f0
oddlama <oddlama@oddlama.org>
2025-04-17 11:54:18 +0200

    chore: add defaults, clarify descriptions, note todos

--- embedded-devices/src/devices/sensirion/scd4x/address.rs
+++ embedded-devices/src/devices/sensirion/scd4x/address.rs
@@ -4,10 +4,10 @@ const ADDRESS: u8 = 0x62;
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug, Format)]
 pub enum Address {
-    /// default address
+    /// Default address
     Default,
     /// Custom address not directly supported by the device, but may be useful
-    /// when using I2c address translators.
+    /// when using I2C address translators.
     Custom(u8),
 }
 
--- embedded-devices/src/devices/sensirion/scd4x/mod.rs
+++ embedded-devices/src/devices/sensirion/scd4x/mod.rs
@@ -1,25 +1,17 @@
-//! # SCD4X(SCD40/SCD41/SCD43)
-//! The SCD4x is Sensirion’s second generation optical CO2
-//! sensor. This sensor builds on the photoacoustic NDIR
-//! sensing principle and Sensirion’s patented PASens® and
-//! CMOSens® technology to offer high accuracy at an
-//! attractive price and small form factor. SMD assembly
-//! allows cost- and space-effective integration of the sensor
-//! combined with maximal freedom of design. On-chip
-//! signal compensation is realized with the built-in SHT4x
-//! humidity and temperature sensor.
+//! # SCD4x (SCD40/SCD41/SCD43)
+//!
+//! The SCD4x is Sensirion's second generation series of optical CO2 sensors. The sensor series
+//! builds on the photoacoustic NDIR sensing principle and Sensirion's patented PASens® and
+//! CMOSens® technology to offer accuracy at an attractive price and small form factor. SMD
+//! assembly allows cost- and space-effective integration of the sensor combined with maximal
+//! freedom of design. On-chip signal compensation is realized with the built-in SHT4x humidity and
+//! temperature sensor.
 //!
 //! Product Variants
-//! ▪ SCD40: Base accuracy, specified measurement
-//! range 400 –2’000 ppm
-//! ▪ SCD41: High accuracy, specified measurement
-//! range 400 – 5’000 ppm, compatible with
-//! California Title 241, RESET® 2 and WELL Building
-//! Standard™ 3, features single shot operation mode
-//! SCD43: High accuracy, specified measurement range
-//! 400 – 5’000 ppm, additionally compatible with
-//! ASHRAE Standard 62.1 4, features single-shot
-//! operation mode
+//! - **SCD40:** Base accuracy, specified range 400 – 2,000 ppm
+//! - **SCD41:** Improved accuracy, specified range 400 – 5,000 ppm, single shot operation feature
+//! - **SCD43:** High accuracy, specified range 400 – 5,000 ppm, comprehensive building standard compatibility, single shot operation feature
+//!
 //! ## Usage (sync)
 //!
 //! ```rust, only_if(sync)
@@ -67,7 +59,6 @@
 //! # Ok(())
 //! # }
 //! ```
-//!
 
 /// TODO: forced field calibration.
 use crc::{Algorithm, CRC_8_NRSC_5};
@@ -89,7 +80,7 @@ use uom::si::{
 pub mod address;
 pub mod registers;
 
-/// Enum describing the different modes the sensor can be in
+/// The different states the sensor can be in
 #[derive(Debug, defmt::Format, Default, PartialEq, Clone)]
 pub enum SensorState {
     #[default]
@@ -99,7 +90,7 @@ pub enum SensorState {
     Sleep,
 }
 
-/// Enum desrcribing which sensor variant we are dealing with
+/// The sensor variant we are dealing with
 #[derive(Default, PartialEq)]
 pub enum SensorVariant {
     #[default]
@@ -107,56 +98,6 @@ pub enum SensorVariant {
     SCD40,
     SCD41,
     SCD43,
-}
-
-#[device]
-#[maybe_async_cfg::maybe(
-    idents(hal(sync = "embedded_hal", async = "embedded_hal_async"), RegisterInterface),
-    sync(feature = "sync"),
-    async(feature = "async")
-)]
-pub struct SCD4x<I: embedded_registers::RegisterInterface> {
-    interface: I,
-    pub variant: SensorVariant,
-    /// The current state this sensor is in
-    /// You should use the `change_state`
-    /// for chaning states to prevent
-    /// a mismatch with the actual state
-    pub state: SensorState,
-}
-
-#[derive(Default)]
-pub struct SCD4xCrcCodec {}
-
-impl Crc8Algorithm for SCD4xCrcCodec {
-    fn new() -> &'static Algorithm<u8> {
-        const CUSTOM_ALG: crc::Algorithm<u8> = CRC_8_NRSC_5;
-        &CUSTOM_ALG
-    }
-}
-type SCD4xCodec = embedded_registers::i2c::codecs::Crc8Codec<2, 2, SCD4xCrcCodec>;
-
-#[maybe_async_cfg::maybe(
-    idents(hal(sync = "embedded_hal", async = "embedded_hal_async"), I2cDevice),
-    sync(feature = "sync"),
-    async(feature = "async")
-)]
-impl<I> SCD4x<embedded_registers::i2c::I2cDevice<I, hal::i2c::SevenBitAddress, SCD4xCodec>>
-where
-    I: hal::i2c::I2c<hal::i2c::SevenBitAddress> + hal::i2c::ErrorType,
-{
-    #[doc = "Initializes a new device with the given address on the specified bus."]
-    #[doc = "This consumes the I2C bus `I`."]
-    #[doc = ""]
-    #[doc = "Before using this device, you should call the [`Self::init`] method which ensures that the device is working correctly."]
-    #[inline]
-    pub fn new_i2c(interface: I, address: self::address::Address) -> Self {
-        Self {
-            interface: embedded_registers::i2c::I2cDevice::new(interface, address.into()),
-            variant: SensorVariant::default(),
-            state: SensorState::default(),
-        }
-    }
 }
 
 /// Measurement data
@@ -178,14 +119,65 @@ pub enum Error<BusError> {
     InvalidSensorVariant(u16),
     /// Trying to run a command which this sensor does not support
     /// e.g. measure_single_shot on SCD40
-    SensorVariantNotSupported(),
+    SensorVariantNotSupported,
     /// Sensor is in a status that does not allow the command you're trying to
     /// run or the command is not sensible for the current state
     InvalidSensorState(SensorState),
     /// The data is not ready yet.
     DataNotReady,
-    /// NVM data copy is still in progress.
-    NvmCopyInProgress,
+}
+
+#[derive(Default)]
+pub struct SCD4xCrcCodec {}
+
+impl Crc8Algorithm for SCD4xCrcCodec {
+    fn new() -> &'static Algorithm<u8> {
+        const CUSTOM_ALG: crc::Algorithm<u8> = CRC_8_NRSC_5;
+        &CUSTOM_ALG
+    }
+}
+type SCD4xCodec = embedded_registers::i2c::codecs::Crc8Codec<2, 2, SCD4xCrcCodec>;
+
+/// The SCD4x is Sensirion's second generation series of optical CO2 sensors. Several product variants
+/// exist with accuracy ranges from 400 – 2,000 ppm (SCD40) up to 400 – 5,000 ppm (SCD43).
+///
+/// For a full description and usage examples, refer to the [module documentation](self).
+#[device]
+#[maybe_async_cfg::maybe(
+    idents(hal(sync = "embedded_hal", async = "embedded_hal_async"), RegisterInterface),
+    sync(feature = "sync"),
+    async(feature = "async")
+)]
+pub struct SCD4x<I: embedded_registers::RegisterInterface> {
+    interface: I,
+    pub variant: SensorVariant,
+    /// The current state this sensor is in. Please use [`Self::change_state`] to enter a different
+    /// state, so the sensor state is known at all times.
+    pub state: SensorState,
+}
+
+#[maybe_async_cfg::maybe(
+    idents(hal(sync = "embedded_hal", async = "embedded_hal_async"), I2cDevice),
+    sync(feature = "sync"),
+    async(feature = "async")
+)]
+impl<I> SCD4x<embedded_registers::i2c::I2cDevice<I, hal::i2c::SevenBitAddress, SCD4xCodec>>
+where
+    I: hal::i2c::I2c<hal::i2c::SevenBitAddress> + hal::i2c::ErrorType,
+{
+    /// Initializes a new device with the given address on the specified bus.
+    /// This consumes the I2C bus `I`.
+    ///
+    /// Before using this device, you should call the [`Self::init`] method which
+    /// initializes the device and ensures that it is working correctly.
+    #[inline]
+    pub fn new_i2c(interface: I, address: self::address::Address) -> Self {
+        Self {
+            interface: embedded_registers::i2c::I2cDevice::new(interface, address.into()),
+            variant: SensorVariant::default(),
+            state: SensorState::default(),
+        }
+    }
 }
 
 #[device_impl]
@@ -195,29 +187,30 @@ pub enum Error<BusError> {
     async(feature = "async")
 )]
 impl<I: embedded_registers::RegisterInterface> SCD4x<I> {
-    /// Initialize the sensor by waiting for the boot-up period and verifying its device id.
-    /// The datasheet specifies a power-on-reset time of 30ms.
-    /// Calling this function is not mandatory, but recommended to ensure proper operation.
+    /// Initialize the sensor by waiting for the boot-up period and verifying its device id. The
+    /// datasheet specifies a power-on-reset time of 30ms. Calling this function is not mandatory,
+    /// but recommended to ensure proper operation.
     pub async fn init<D: hal::delay::DelayNs>(&mut self, delay: &mut D) -> Result<(), Error<I::Error>> {
         delay.delay_ms(30).await;
         let device_id = self.read_register::<GetSensorVariant>().await.map_err(Error::Bus)?;
-        if (device_id.read_variant() & 0xF000) != self::registers::SCD40_ID {
+        if (device_id.read_variant() & 0xF000) != self::registers::SENSOR_VARIANT_SCD40 {
             self.variant = SensorVariant::SCD40
-        } else if (device_id.read_variant() & 0xF000) != self::registers::SCD41_ID {
+        } else if (device_id.read_variant() & 0xF000) != self::registers::SENSOR_VARIANT_SCD41 {
             self.variant = SensorVariant::SCD41
-        } else if (device_id.read_variant() & 0xF000) != self::registers::SCD43_ID {
+        } else if (device_id.read_variant() & 0xF000) != self::registers::SENSOR_VARIANT_SCD43 {
             self.variant = SensorVariant::SCD43
         };
         if self.variant == SensorVariant::Unknown {
             return Err(Error::InvalidSensorVariant(device_id.read_variant()));
         }
 
+        // TODO: run "perform self test"?
         Ok(())
     }
 
-    /// Set the current sensor altitude is meters above the sea level
-    /// should be between 0 and 3000
-    /// Can only be set while in idle mode
+    /// Sets the current sensor altitude in meters above sea level. This value is used to derive an
+    /// expected air pressure value which will be used in signal compensation. Expects a value
+    /// between 0 and 3000 meters. This function must be called while the sensor is in idle mode.
     pub async fn set_sensor_altitude(&mut self, height: &mut Length) -> Result<(), Error<I::Error>> {
         if self.state != SensorState::Idle {
             return Err(Error::InvalidSensorState(self.state.clone()));
@@ -229,9 +222,9 @@ impl<I: embedded_registers::RegisterInterface> SCD4x<I> {
         Ok(())
     }
 
-    /// Set the sensor ambient pressure
-    /// should be between 70000 to 120000 Pa
-    /// Can only be set while in periodic measurement mode
+    /// Sets the ambient pressure which will be used for live signal compensation. A value between
+    /// 70,000 to 120,000 Pa is expected. This function must be called while the sensor is in
+    /// periodic measurement mode.
     pub async fn set_sensor_ambient_pressure(&mut self, height: &mut Pressure) -> Result<(), Error<I::Error>> {
         if self.state != SensorState::PeriodicMeasurement || self.state != SensorState::LowPowerPeriodicMeasurement {
             return Err(Error::InvalidSensorState(self.state.clone()));
@@ -243,12 +236,12 @@ impl<I: embedded_registers::RegisterInterface> SCD4x<I> {
         Ok(())
     }
 
-    /// Unconditionally try and get a measurement, returning an error if no data is
-    /// ready
+    /// Waits for the next measurement result and returns it.
+    /// TODO: this must wait for data ready! a measurement can only be read out
+    /// once, the buffer is cleared according to the datasheet
     async fn get_measurement(&mut self) -> Result<Measurements, Error<I::Error>> {
         let measurements = self.read_register::<ReadMeasurement>().await.map_err(Error::Bus)?;
-        let co2 = measurements.read_co2();
-        let co2 = Rational32::new_raw(co2 as i32, 1);
+        let co2 = Rational32::new_raw(measurements.read_co2() as i32, 1);
         let co2 = Ratio::new::<part_per_million>(co2);
         let temperature = Rational32::new_raw(175 * measurements.read_temperature() as i32, (1 << 16) - 1);
         let temperature = temperature - 45;
@@ -262,6 +255,16 @@ impl<I: embedded_registers::RegisterInterface> SCD4x<I> {
         })
     }
 
+    // TODO: implement function to reset the state by issuing wake up and
+    // stop_periodic_measurement.
+
+    /// Switch sensor state. This function will issue the correct command to switch from the
+    /// current sensor state to the desired state. The SCD4x series has no readout for the current
+    /// internal state, this function relies on knowing the current state. If the current state is
+    /// not known, please reset the sensor by issuing wake-up and stop-periodic-measurement
+    /// manually.
+    // TODO: ^------- implement force parameter to do that automatically? call reset_state or
+    // something.
     pub async fn change_state<D: hal::delay::DelayNs>(
         &mut self,
         delay: &mut D,
@@ -284,7 +287,7 @@ impl<I: embedded_registers::RegisterInterface> SCD4x<I> {
             }
             (SensorState::Idle, SensorState::Sleep) => {
                 if self.variant == SensorVariant::SCD40 {
-                    Err(Error::SensorVariantNotSupported())
+                    Err(Error::SensorVariantNotSupported)
                 } else {
                     self.write_register(PowerDown::default()).await.map_err(Error::Bus)?;
                     self.state = state;
@@ -312,7 +315,7 @@ impl<I: embedded_registers::RegisterInterface> SCD4x<I> {
             }
             (SensorState::PeriodicMeasurement, SensorState::Sleep) => {
                 if self.variant == SensorVariant::SCD40 {
-                    Err(Error::SensorVariantNotSupported())
+                    Err(Error::SensorVariantNotSupported)
                 } else {
                     self.write_register(StopPeriodicMeasurement::default())
                         .await
@@ -344,7 +347,7 @@ impl<I: embedded_registers::RegisterInterface> SCD4x<I> {
             }
             (SensorState::LowPowerPeriodicMeasurement, SensorState::Sleep) => {
                 if self.variant == SensorVariant::SCD40 {
-                    Err(Error::SensorVariantNotSupported())
+                    Err(Error::SensorVariantNotSupported)
                 } else {
                     self.write_register(StopPeriodicMeasurement::default())
                         .await
@@ -357,7 +360,7 @@ impl<I: embedded_registers::RegisterInterface> SCD4x<I> {
             }
             (SensorState::Sleep, SensorState::Idle) => {
                 if self.variant == SensorVariant::SCD40 {
-                    Err(Error::SensorVariantNotSupported())
+                    Err(Error::SensorVariantNotSupported)
                 } else {
                     self.write_register(WakeUp::default()).await.map_err(Error::Bus)?;
                     delay.delay_ms(30).await;
@@ -367,7 +370,7 @@ impl<I: embedded_registers::RegisterInterface> SCD4x<I> {
             }
             (SensorState::Sleep, SensorState::PeriodicMeasurement) => {
                 if self.variant == SensorVariant::SCD40 {
-                    Err(Error::SensorVariantNotSupported())
+                    Err(Error::SensorVariantNotSupported)
                 } else {
                     self.write_register(WakeUp::default()).await.map_err(Error::Bus)?;
                     delay.delay_ms(30).await;
@@ -380,7 +383,7 @@ impl<I: embedded_registers::RegisterInterface> SCD4x<I> {
             }
             (SensorState::Sleep, SensorState::LowPowerPeriodicMeasurement) => {
                 if self.variant == SensorVariant::SCD40 {
-                    Err(Error::SensorVariantNotSupported())
+                    Err(Error::SensorVariantNotSupported)
                 } else {
                     self.write_register(WakeUp::default()).await.map_err(Error::Bus)?;
                     delay.delay_ms(30).await;
@@ -395,11 +398,9 @@ impl<I: embedded_registers::RegisterInterface> SCD4x<I> {
         }
     }
 
-    // Performs a one-shot measurement. This will set the conversion mode to [`self::registers::ConversionMode::Oneshot´].
-    // which will cause the device to perform a single conversion a return to sleep mode afterwards.
-    //
-    // This function will initialize the measurement, wait until the data is acquired and return
-    // the temperature.
+    // Performs a one-shot measurement. If the sensor supports single shot measurements, we will
+    // utilize this, otherwise a single shot measurement is emulated by starting and stopping
+    // periodic measurement.
     pub async fn measure<D: hal::delay::DelayNs>(&mut self, delay: &mut D) -> Result<Measurements, Error<I::Error>> {
         match self.state {
             SensorState::Idle => match self.variant {
@@ -412,15 +413,17 @@ impl<I: embedded_registers::RegisterInterface> SCD4x<I> {
                 }
                 _ => {
                     self.change_state(delay, SensorState::PeriodicMeasurement).await?;
+                    // TODO: wait for data ready instead of 5000ms?
                     delay.delay_ms(5000).await;
-                    let erg = self.get_measurement().await;
+                    let ret = self.get_measurement().await;
                     self.change_state(delay, SensorState::Idle).await?;
-                    erg
+                    ret
                 }
             },
             SensorState::PeriodicMeasurement => {
                 let mut iteration = 0;
                 loop {
+                    // TODO: extract this into a function that waits for the next measurement?
                     let ready = self.read_register::<GetDataReadyStatus>().await.map_err(Error::Bus)?;
                     if (ready.read_status() & DATA_READY_MASK) != 0 {
                         return self.get_measurement().await;
@@ -445,18 +448,17 @@ impl<I: embedded_registers::RegisterInterface> SCD4x<I> {
                 }
             }
             SensorState::Sleep => {
-                // Sleep is only supported on the SCD41 or SCD43
-                // since both support single shot measurements we
-                // start one without further guards
+                // Sleep is only supported on the SCD41 or SCD43 since both support single shot
+                // measurements we start one without further guards
                 self.change_state(delay, SensorState::Idle).await?;
                 self.write_register(MeasureSingleShot::default())
                     .await
                     .map_err(Error::Bus)?;
                 delay.delay_ms(5000).await;
-                let erg = self.get_measurement().await;
+                let ret = self.get_measurement().await;
                 self.change_state(delay, SensorState::Idle).await?;
                 self.change_state(delay, SensorState::Sleep).await?;
-                erg
+                ret
             }
         }
     }
